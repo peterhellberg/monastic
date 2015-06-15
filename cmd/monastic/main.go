a3468ec46032f0f6239f93c0a3d8e5150f323a84
Peter Hellberg <peter@c7.se>
2015-06-15 03:06:03 +0200

    Also output the number and parts used for cipher

--- cmd/monastic/main.go
+++ cmd/monastic/main.go
@@ -12,7 +12,7 @@ func main() {
 	if len(os.Args) > 1 {
 		if n, err := strconv.Atoi(os.Args[1]); err == nil {
 			if c, err := monastic.Cipher(n); err == nil {
-				fmt.Printf("\n%s\n", c)
+				fmt.Printf("%d %v\n\n%s\n", n, c.Parts(), c)
 			}
 		}
 	}
